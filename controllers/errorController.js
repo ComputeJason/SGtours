a36143c4f2b762d8db9a43082b31b5395b1cef50
ComputeJason <jasonchenys1998@gmail.com>
2023-10-02 22:01:16 +0800

    add base error handling pt1.

---
+++ controllers/errorController.js
@@ -0,0 +1,9 @@
+// 4 params express knows its a error handling middleware! cool
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
